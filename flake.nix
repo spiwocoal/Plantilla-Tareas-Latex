1fef830fd9d9f6939a2ef0eb6b2dbe8855efbc95
Bruno Pacheco <spiwocoal@gmail.com>
2025-05-19 13:46:43 -0400

    Titulos de sub(sub)secciones, externalización

--- flake.nix
+++ flake.nix
@@ -4,51 +4,77 @@
     nixpkgs.url = github:NixOS/nixpkgs/nixos-24.05;
     utils.url = github:numtide/flake-utils;
   };
-  outputs = { self, nixpkgs, utils, ... }:
-    utils.lib.eachDefaultSystem (system:
-      let
+  outputs = {
+    self,
+    nixpkgs,
+    utils,
+    ...
+  }:
+    utils.lib.eachDefaultSystem (
+      system: let
         overlays = [
           (self: super: rec {
-            python = (super.python3.withPackages (python-pkgs: with python-pkgs; [
-              pygments catppuccin
-            ]));
-            tex = (super.texlive.withPackages (ps: with ps; [
-              texdoc # recommended package to navigate the documentation
-              scheme-medium # most packages you will need
+            python = super.python3.withPackages (python-pkgs:
+              with python-pkgs; [
+                pygments
+                catppuccin
+              ]);
+            tex = super.texlive.withPackages (ps:
+              with ps; [
+                texdoc # recommended package to navigate the documentation
+                scheme-medium # most packages you will need
 
-              # paquetes necesarios para la plantilla
-              biber biblatex ccicons csquotes doclicense ebgaramond enumitem 
-              fbox float ifmtarg newtx pict2e subfiles thmtools titlesec
-              titling transparent xifthen xstring 
+                # paquetes necesarios para la plantilla
+                biber
+                biblatex
+                ccicons
+                csquotes
+                doclicense
+                ebgaramond
+                enumitem
+                fbox
+                float
+                ifmtarg
+                newtx
+                pict2e
+                subfiles
+                thmtools
+                titlesec
+                titling
+                transparent
+                xifthen
+                xstring
 
-              # paquetes necesarios para minted
-              minted upquote
+                # paquetes necesarios para minted
+                minted
+                upquote
 
-              # paquetes para generar gráficos
-              pgfplots
+                # paquetes para generar gráficos
+                pgfplots
 
-              # otros paquetes útiles
-              placeins
-              
-              lipsum
-            ]));
+                # otros paquetes útiles
+                placeins
+
+                lipsum
+              ]);
           })
         ];
-        pkgs = import nixpkgs { inherit system overlays; };
+        pkgs = import nixpkgs {inherit system overlays;};
       in rec {
         devShells.default = pkgs.mkShell {
-          packages = with pkgs; [ python tex ];
+          packages = with pkgs; [python tex];
         };
 
         packages = {
           document = pkgs.stdenvNoCC.mkDerivation rec {
             name = "latex-udec";
             src = self;
-            buildInputs = with pkgs; [ coreutils ghostscript ncurses which python tex ];
+            buildInputs = with pkgs; [coreutils ghostscript ncurses which python tex];
             phases = ["unpackPhase" "buildPhase" "installPhase"];
             buildPhase = ''
               export PATH="$PATH:${pkgs.lib.makeBinPath buildInputs}";
               mkdir -p .cache/texmf-var
+              mkdir -p build
               env TEXMFHOME=.cache TEXMFVAR=.cache/texmf-var \
                 latexmk -interaction=nonstopmode -pdf -lualatex -bibtex -shell-escape \
                   main.tex
